c82573cfa8359c210dc15f6259869055cd50819a
mcleanlu <lucas.w.mclean@gmail.com>
2023-07-03 04:53:11 -0600

    commit

---
+++ C_Sharp_password_manager/main.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class PasswordEntity
+{
+    public string ID { get; set; }
+    public string Password { get; set; }
+    public string Url { get; set; }
+
+    public PasswordEntity(string url, string id, string password)
+    {
+        Url = url;
+        ID = id;
+        Password = password;
+    }
+}
+
+struct FileOperations
+{
+    public string FilePath { get; set; }
+
+    public void WriteToFile(List<PasswordEntity> entities)
+    {
+        using (StreamWriter sw = new StreamWriter(FilePath))
+        {
+            foreach (var entity in entities)
+            {
+                sw.WriteLine($"{entity.Url},{entity.ID},{entity.Password}");
+            }
+        }
+    }
+
+    public List<PasswordEntity> ReadFromFile()
+    {
+        var entities = new List<PasswordEntity>();
+        using (StreamReader sr = new StreamReader(FilePath))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] details = line.Split(',');
+                entities.Add(new PasswordEntity(details[0], details[1], details[2]));
+            }
+        }
+        return entities;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FileOperations fileOperations = new FileOperations { FilePath = "passwords.txt" };
+        List<PasswordEntity> passwordEntities = fileOperations.ReadFromFile();
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Add a website");
+            Console.WriteLine("2. View a website");
+            Console.WriteLine("3. Delete a website");
+            Console.WriteLine("4. Exit");
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.Write("Enter website address: ");
+                    string url = Console.ReadLine();
+                    Console.Write("Enter username: ");
+                    string id = Console.ReadLine();
+                    Console.Write("Enter password: ");
+                    string password = Console.ReadLine();
+                    passwordEntities.Add(new PasswordEntity(url, id, password));
+                    fileOperations.WriteToFile(passwordEntities);
+                    break;
+                case "2":
+                    if (passwordEntities.Count == 0)
+                    {
+                        Console.WriteLine("No websites found.");
+                        break;
+                    }
+
+                    Console.WriteLine("Websites:");
+                    foreach (var e in passwordEntities)
+                    {
+                        Console.WriteLine(e.Url);
+                    }
+
+                    Console.Write("Enter website address to view: ");
+                    string viewUrl = Console.ReadLine();
+                    PasswordEntity entityToView = passwordEntities.Find(e => e.Url == viewUrl);
+                    if (entityToView != null)
+                    {
+                        Console.WriteLine($"Username: {entityToView.ID}");
+                        Console.WriteLine($"Password: {entityToView.Password}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Website not found.");
+                    }
+                    break;
+                case "3":
+                    Console.Write("Enter website address to delete: ");
+                    string deleteUrl = Console.ReadLine();
+                    PasswordEntity deleteEntity = passwordEntities.Find(e => e.Url == deleteUrl);
+                    if (deleteEntity != null)
+                    {
+                        passwordEntities.Remove(deleteEntity);
+                        fileOperations.WriteToFile(passwordEntities);
+                        Console.WriteLine("Website deleted.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Website not found.");
+                    }
+                    break;
+                case "4":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please enter a number between 1 and 4.");
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Overview
+
+As a software engineer, I am continuously striving to deepen my knowledge of various programming languages, particularly C#. To this end, I developed a basic console-based password manager to familiarize myself with C#'s syntax, and object-oriented programming principles.
+
+This password manager is a console application written in C#. It allows users to add, view, and delete website entries along with their associated usernames and passwords. The data is persisted in a text file, providing basic functionality of a file-based database.
+
+The primary purpose of writing this software was to demonstrate my understanding of key programming concepts, including variables, expressions, conditionals, loops, functions, classes, file I/O operations, and structures in the C# language.
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I used Visual Studio Code as my text editor, equipped with the C# plugin for syntax highlighting and IntelliSense. For building and executing the program, I used .NET Core's Command Line Interface (CLI).
+
+The programming language used is C#, and the .NET Core framework provided the libraries used in the program, such as System, System.Collections.Generic, and System.IO.
+
+# Useful Websites
+
+- [Microsoft's C# Documentation](https://docs.microsoft.com/en-us/dotnet/csharp/)
+- [Dot Net Perls](https://www.dotnetperls.com/)
+- [Stack Overflow](https://stackoverflow.com/)
+
+# Future Work
+
+There are several enhancements that can be made to this application in the future:
+
+* Add data encryption for secure storage of usernames and passwords.
+* Add user authentication to access the password manager.
+* Improve user interface for better user experience.
+* Integrate with a real database for better data management and integrity.
+* Handle exceptions and edge cases to make the program more robust.
